a51906e2a4ffb74ad4d699f00e502e2e0d9a574f
Vedran Stojnović <phidrho@gmail.com>
2023-03-20 18:47:33 +0100

    dodano u skriptu računanje u slova u rečenici i dodan test za slovo e

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -1,9 +1,16 @@
-const sum = require('../script');
+const mojModul = require('../script');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(mojModul.sum(1, 2)).toBe(3);
 });
 
 test('adds 2 + 2 to equal 4', () => {
-  expect(sum(2, 2)).toEqual(4);
+  expect(mojModul.sum(2, 2)).toEqual(4);
+});
+
+
+let mojaRecenica = 'Moja recenica sadrzi 3 slova e';
+
+test('ima 3 slova e u rečenici', () => {
+  expect(mojModul.brojSlova(mojaRecenica, 'e')).toEqual(3);
 });
