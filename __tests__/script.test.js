f1e8f5176907977b4cbfbb1bee5e548dc62577c0
Vedran Stojnović <phidrho@gmail.com>
2023-03-20 17:45:20 +0100

    reorganizirano u mapu tests i dodan test

---
+++ __tests__/script.test.js
@@ -0,0 +1,9 @@
+const sum = require('../script');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 + 2 to equal 3', () => {
+  expect(sum(2, 2)).toEqual(4);
+});
